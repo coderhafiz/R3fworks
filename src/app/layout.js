c53c9052cc85aaf2ac873e36eaa2bc3e1b798713
Coderhafiz <hafizabd457@gmail.com>
2025-09-05 08:00:00 +0100

    edit to a bunch of files again

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="preload-models" strategy="beforeInteractive">
+          {`
+            const models = ["/compModel.glb"];
+            models.forEach(url => fetch(url));
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-white dark:bg-black">
+      <p className="text-9xl font-extrabold text-black dark:text-white">
+        loading..
+      </p>
+    </div>
+  );
+}
